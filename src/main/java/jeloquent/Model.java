796b6773d1b3fbbb50fc6725ba2e6046ef9f9ad2
Francis <lagson6@yahoo.fr>
2016-03-09 17:37:03 -0800

    This is the model class with constructor , setters and getters

---
+++ src/main/java/jeloquent/Model.java
@@ -0,0 +1,34 @@
+package jeloquent;
+
+/**
+ * Created by Francis on 3/7/16.
+ */
+public class Model {
+
+    private static String primaryKey;
+    private static String table;
+
+    public Model() {
+        Model.table = "";
+        Model.primaryKey = "";
+    }
+    public Model(String table, String key){
+        Model.table = table;
+        Model.primaryKey = key;
+    }
+    public static String getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public static void setPrimaryKey(String primaryKey) {
+        Model.primaryKey = primaryKey;
+    }
+
+    public static String getTable() {
+        return table;
+    }
+
+    public static void setTable(String table) {
+        Model.table = table;
+    }
+}
