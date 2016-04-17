fc7c6ce41df6afc41049fd57c73ee35925ec533d
Francis <lagson6@yahoo.fr>
2016-04-16 21:21:08 -0700

    Merge remote-tracking branch 'remotes/origin/dev'
    # Conflicts:
    #	Jeloquent.iml
    #	src/main/java/jeloquent/Model.java

--- src/main/java/jeloquent/Collection.java
+++ src/main/java/jeloquent/Collection.java
@@ -1,5 +1,15 @@
-package Jeloquent;
-import java.util.*;
+package jeloquent;
+
+import org.json.*;
+import com.google.gson.*;
+
+
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by sargis on 4/3/2016.
@@ -8,6 +18,7 @@ public class Collection {
     private ArrayList<Model> Collection = new ArrayList<Model>();
     private int numOfModels;
 
+    private Builder builder = new Builder();
 
     Collection() {
         numOfModels = 0;
@@ -21,7 +32,7 @@ public class Collection {
     public int Count() {
         int count = 0;
         Iterator itr = Collection.iterator();
-        while(itr.hasNext()){
+        while (itr.hasNext()) {
             itr.next();
             count++;
         }
@@ -29,9 +40,37 @@ public class Collection {
     }
 
     public boolean isEmpty() {
-        if(numOfModels == 0)
+        if (numOfModels == 0)
             return true;
         else
             return false;
+    }
+
+    public void toJson() throws SQLException, JSONException {
+
+        ResultSet result = builder.runQuery("SELECT * FROM " + builder.table);
+        JSONArray jar = new JSONArray();
+        JSONObject obj = new JSONObject();
+        JSONArray lst = new JSONArray();
+
+        ResultSetMetaData meta = result.getMetaData();
+        int numberOfColumn = meta.getColumnCount();
+        for(int i = 0; i<=numberOfColumn; i++){
+            jar.put(String.valueOf(meta.getColumnName(i)));
+            obj.put("Column", jar);
+        }
+        System.out.println();
+
+        while (result.next()){
+            for (int i =1; i<=numberOfColumn; i++){
+                lst.put(String.valueOf(result.getObject(i)));
+                obj.put("",lst);
+            }
+        }
+        System.out.println();
+        Gson gson = new Gson();
+        String mes = gson.toJson(obj);
+        System.out.println(builder.table + ":"+mes);
+
     }
 }
--- src/main/java/jeloquent/Model.java
+++ src/main/java/jeloquent/Model.java
@@ -1,52 +1,49 @@
 package jeloquent;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
- * Created by Francis on 3/7/16.
+ * Created by shant on 4/6/16.
  */
-public class Model {
+public abstract class Model {
 
-    private static String primaryKey;
-    private static String table;
-    private static Map<String, String> model;
+    protected String table;
+    protected String[] fields = {"id", "body", "title"};
+    protected Builder builder;
 
     public Model() {
-        Model.table = "";
-        Model.primaryKey = "";
-    }
-    public Model(String table, String key){
-        Model.table = table;
-        Model.primaryKey = key;
-    }
-    public static String getPrimaryKey() {
-        return primaryKey;
+        this.builder = new Builder("posts", this.fields);
     }
 
-    public static void setPrimaryKey(String primaryKey) {
-        Model.primaryKey = primaryKey;
+    public Map find(Integer id) {
+        return this.builder.find(1);
     }
 
-    public static String getTable() {
-        return table;
+    public Map find(Integer id, String[] fields) {
+        return this.builder.find(id, fields);
     }
 
-    public static void setTable(String table) {
-        Model.table = table;}
-    protected static void create(String table, String pk){
-        model = new HashMap<>() ;
-        model.put(table, pk);
 
+    public ArrayList all() {
+        return this.builder.all();
     }
 
-    protected void find(String key){}
-    protected void destroy(String [] keys){}
-    protected QueryBuilder where(String field, String operator, String value){}
-    protected QueryBuilder hasOne(String relationship){}
-    protected QueryBuilder hasMany(String relationship){}
-    protected QueryBuilder belongsTo(String relationship){}
-    protected QueryBuilder belongsToMany(String relationship){}
-    protected QueryBuilder with(String relationship){}
+
+    public ArrayList all(String[] fields) {
+        return this.builder.all(fields);
+    }
+
+    public Builder where(String field, String operator, String value) {
+        return this.builder.where(field, operator, value);
+    }
+
+    public int create(Map fields) {
+        return this.builder.create(fields);
+    }
+
+    public int update(int id, Map fields) {
+        return this.builder.update(id, fields);
+    }
 
 }
